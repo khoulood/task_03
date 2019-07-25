6831ea2e7e48da19c82b76c471ad2b5b40147a18
khoulood alharthi <khouloodalharthi@khouloods-MacBook-Pro.local>
2019-07-25 14:58:53 +0300

    task3 finally

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,15 +5,19 @@ def welcome(request):
 
 def restaurant_list(request):
 
-    context = {
+    context = { "my_list": [ {"restaurant_name":"albaik","food_type":"spaicy"},
+                             {"restaurant_name":"pizza hot","food_type":"margrita"},
+                             {"restaurant_name":"steak house","food_type":"spaicy"},
+                            ],
 
-    }
+               }
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
-    context = {
+    context = { 
+             "my_object": {"restaurant_name":"albaik","food_type":"spaicy"},
 
     }
     return render(request, 'detail.html', context)
